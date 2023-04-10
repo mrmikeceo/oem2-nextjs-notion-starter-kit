10f1b9c48c010b0f82d30a96b0b67f507607a2eb
mrmikeceo <36260953+mrmikeceo@users.noreply.github.com>
2023-04-10 18:02:13 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,8 +12,8 @@ module.exports = withBundleAnalyzer({
       'images.unsplash.com',
       'pbs.twimg.com',
       'abs.twimg.com',
-      's3.us-west-2.amazonaws.com',
-      'transitivebullsh.it'
+      'redis-15498.c9.us-east-1-4.ec2.cloud.redislabs.com',
+      'oem2.com'
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
