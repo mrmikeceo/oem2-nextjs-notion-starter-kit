0c55a4194cf159fb6353619ffbfaf8e79028ec0a
mrmikeceo <36260953+mrmikeceo@users.noreply.github.com>
2023-04-10 10:01:30 +0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'OEM2 - All About Technologies',
-  domain: '',
+  domain: 'oem2com.notion.site',
   author: 'Good A. Brain',
 
   // open graph metadata (optional)
